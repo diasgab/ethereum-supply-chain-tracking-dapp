4b7f0a42d49b3c5762406aea4c7314b20180fa14
Gabriel Dias <diasgab@gmail.com>
2021-06-05 10:15:25 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+copy the .env.dist file to .env
+
+
+Truffle version:
+Truffle v5.3.6 (core: 5.3.6)
+Solidity - 0.5.16 (solc-js)
+Node v12.16.1
+Web3.js v1.3.5
---
+++ truffle-config.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const infuraKey = process.env.INFURA_KEY;
+const mnemonic = process.env.MNEMONIC;
+
+module.exports = {
+
+  networks: {
+
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 9545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+      gas: 4500000,        
+      gasPrice: 10000000000,
+    },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,       // rinkeby's id
+      gas: 4500000,        // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000,
+      skipDryRun: true
+    },
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.4.24"
+    }
+  }
+}
